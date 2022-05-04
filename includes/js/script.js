33fa71d3dc87e950b1d86b108aa030f90fc1010c
Erica Guadalupe <ericaguadalupe511@gmail.com>
2022-05-04 19:08:12 -0400

    First commit

--- includes/js/script.js
+++ includes/js/script.js
@@ -1,12 +1,12 @@
 /* KEY */ 
 
 // 1 - Calendar 
-// 2 - Pagination 
+// 2 - Side Navbar 
 // 3 - Popup Modal 
-// 4 - HTTP Requests 
+// 4 - Pagination (To be done once back end is in completion) 
 
 
-/* CALENDAR */ 
+/* ----- CALENDAR ------- */ 
 
 
 let calendar = document.querySelector('.calendar')
@@ -78,7 +78,7 @@ month_names.forEach((e, index) => {
 
 /* This is the onclick that shows the months */ 
 
-let month_picker = calendar.querySelector('#month-picker')
+let month_picker = calendar.querySelector('.month-picker');
 
 month_picker.onclick = () => {
     month_list.classList.add('show')
@@ -101,9 +101,143 @@ document.querySelector('#next-year').onclick = () => {
     generateCalendar(curr_month.value, curr_year.value)
 }
 
-// other JS --- jQuery 
-// $(function() {
-//     alert("I see you've reloaded");
-// });
+/* ----  SIDE NAVBAR  -----*/ 
 
-/* ----  NAVBAR  -----*/ 
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.navbar');
+
+    
+    burger.addEventListener('click',()=>{
+        // toggle nav 
+        nav.classList.toggle('nav-active');
+    });
+}
+
+navSlide();
+
+/* ----------- MODAL -----------*/ 
+
+var modal = document.querySelector('#myModal');
+var btn = document.querySelector('#myBtn');
+var span = document.getElementsByClassName('close')[0];
+
+btn.onclick = function() {
+    modal.style.display = 'block'; 
+}
+span.onclick = function() {
+    modal.style.display = 'none';
+}
+
+// if a user clicks out the window 
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = 'none'; 
+    }
+}
+
+/*--- LIST ----*/
+
+let todoItems = [];
+
+function renderTodo(todo) {
+  localStorage.setItem('todoItems', JSON.stringify(todoItems));
+
+  const list = document.querySelector('.js-todo-list');
+  const item = document.querySelector(`[data-key='${todo.id}']`);
+  
+  if (todo.deleted) {
+    item.remove();
+    return
+  }
+
+  const isChecked = todo.checked ? 'done': '';
+  const node = document.createElement("li");
+  node.setAttribute('class', `todo-item ${isChecked}`);
+  node.setAttribute('data-key', todo.id);
+  node.innerHTML = `
+    <input id="${todo.id}" type="checkbox"/>
+    <label for="${todo.id}" class="tick js-tick"></label>
+    <span>${todo.text}</span>
+    <button class="delete-todo js-delete-todo">
+    <span id="#delete-icon"></span>
+  `;
+
+  if (item) {
+    list.replaceChild(node, item);
+  } else {
+    list.append(node);
+  }
+}
+
+function addTodo(text) {
+  const todo = {
+    text,
+    checked: false,
+    id: Date.now(),
+  };
+
+  todoItems.push(todo);
+  renderTodo(todo);
+}
+
+function toggleDone(key) {
+  const index = todoItems.findIndex(item => item.id === Number(key));
+  todoItems[index].checked = !todoItems[index].checked;
+  renderTodo(todoItems[index]);
+}
+
+function deleteTodo(key) {
+  const index = todoItems.findIndex(item => item.id === Number(key));
+  const todo = {
+    deleted: true,
+    ...todoItems[index]
+  };
+  todoItems = todoItems.filter(item => item.id !== Number(key));
+  renderTodo(todo);
+}
+
+const form = document.querySelector('.js-form');
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const input = document.querySelector('.js-todo-input');
+
+  const text = input.value.trim();
+  if (text !== '') {
+    addTodo(text);
+    input.value = '';
+    input.focus();
+  }
+});
+
+const list = document.querySelector('.js-todo-list');
+list.addEventListener('click', event => {
+  if (event.target.classList.contains('js-tick')) {
+    const itemKey = event.target.parentElement.dataset.key;
+    toggleDone(itemKey);
+  }
+  
+  if (event.target.classList.contains('js-delete-todo')) {
+    const itemKey = event.target.parentElement.dataset.key;
+    deleteTodo(itemKey);
+  }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const ref = localStorage.getItem('todoItems');
+  if (ref) {
+    todoItems = JSON.parse(ref);
+    todoItems.forEach(t => {
+      renderTodo(t);
+    });
+  }
+});
+
+/*--- MODAL DATE TO DISPLAY CURRENT TIME ----*/ 
+
+const d = new Date(); 
+
+document.querySelector('span.date').innerHTML = d.toDateString();
+
+/*------------- PAGINATION -------------*/ 
+
